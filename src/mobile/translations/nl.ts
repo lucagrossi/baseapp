f53d29a29af90d7b341053fdccfbef1d2ad701e3
lucagrossi <76164466+lucagrossi@users.noreply.github.com>
2021-02-20 08:31:21 +0100

    Create nl.ts

---
+++ src/mobile/translations/nl.ts
@@ -0,0 +1,119 @@
+export const en = {
+    'page.mobile.header.signIn': 'Log In',
+    'page.mobile.footer.home': 'Home',
+    'page.mobile.footer.orders': 'Orders',
+    'page.mobile.footer.trading': 'Handel',
+    'page.mobile.footer.wallets': 'Spelers',
+    'page.mobile.footer.profile': 'Profiel',
+
+    'page.mobile.tradingTabs.label.createOrder': 'Maak Order',
+    'page.mobile.tradingTabs.label.charts': 'Grafiek',
+    'page.mobile.tradingTabs.label.orders': 'Orders',
+
+    'page.mobile.charts.label.orderBook': 'Order book',
+    'page.mobile.charts.label.trades': 'Trades',
+
+    'page.mobile.order.price.placeholder': 'Prijs ({currency})',
+    'page.mobile.order.amount.placeholder': 'Aantal ({currency})',
+
+    'page.mobile.orderButtons.buy': 'Koop {base_unit}',
+    'page.mobile.orderButtons.sell': 'Verkoop {base_unit}',
+
+    'page.mobile.orders.tabs.open': 'Open',
+    'page.mobile.orders.tabs.all': 'Alles',
+    'page.mobile.orders.cancelAll': 'Cancel Alles',
+
+    'page.mobile.orders.open.title': 'Open Orders',
+    'page.mobile.orders.header.orderType.buy.market': 'Koop market',
+    'page.mobile.orders.header.orderType.buy.limit': 'Koop limit',
+    'page.mobile.orders.header.orderType.sell.market': 'Verkoop market',
+    'page.mobile.orders.header.orderType.sell.limit': 'Verkoop limit',
+    'page.mobile.orders.header.amount': 'Aantal',
+    'page.mobile.orders.header.price': 'Prijs',
+    'page.mobile.orders.header.filled': 'Uitgevoerd',
+    'page.mobile.orders.header.cancel': 'Cancel',
+
+    'page.mobile.currentMarketInfo.search.placeholder': 'Zoek',
+    'page.mobile.currentMarketInfo.volume': 'Vol',
+    'page.mobile.currentMarketInfo.high': 'Hoog',
+    'page.mobile.currentMarketInfo.low': 'Laag',
+
+    'page.header.signIn.forgotPassword': 'Wachtwoord vergeten?',
+    'page.header.signIN.noAccountYet': 'Nog geen account?',
+    'page.header.signUp.terms': 'Ik ga akkoord met alle voorwaarden',
+    'page.header.signUp.alreadyRegistered': 'Reeds geregistreerd?',
+
+    'page.mobile.reset.header': 'Verstuur bevestiging opnieuw',
+    'page.mobile.signin.kyc.header': '2FA Verificatie',
+
+    'page.mobile.wallets.deposit.history': 'Stortings historie',
+    'page.mobile.wallets.withdraw.history': 'Opname historie',
+    'page.mobile.wallets.banner.total': 'Totaal',
+    'page.mobile.wallets.banner.available': 'Beschikbaar',
+
+    'page.mobile.copy.text': 'Kopieer adres',
+    'page.mobile.wallet.deposit.generate': 'Generereer stortings adres',
+    'page.mobile.wallet.deposit.generating': 'Genereer adres...',
+
+    'page.mobile.wallet.withdraw.modal.new.account': 'Bevestig nieuw account',
+    'page.mobile.wallet.withdraw.modal.confirmation': 'Opname bevestiging',
+    'page.mobile.wallet.withdraw.modal.confirmation.warning': 'Verifieer het doeladres goed voordat je de opname bevestigd.', 
+    'page.mobile.wallet.withdraw.modal.confirmation.message1': 'Je hebt aanvraagd om op te nemen',
+    'page.mobile.wallet.withdraw.modal.confirmation.message2': 'Naar dit adres',
+
+    'page.mobile.userInfo.details.uid': 'UID: {uid}',
+    'page.mobile.userInfo.logout.button': 'Logout',
+    'page.mobile.profileLinks.main.verification': 'Profiel verificatie',
+    'page.mobile.profileLinks.main.2fa': 'Two-factor authenticatie',
+    'page.mobile.profileLinks.main.changePassword': 'Wijzig password',
+    'page.mobile.profileLinks.main.activity': 'Account activiteit',
+    'page.mobile.profileLinks.main.apiKeys': 'Mijn API keys',
+
+    'page.mobile.profileLinks.settings.language': 'Taal',
+    'page.mobile.profileLinks.settings.theme': 'Thema',
+
+    'page.mobile.profileLinks.additional.referral': 'Referentie Link',
+    'page.mobile.profileLinks.link.referral.success': 'Succesvol gekopieerd!',
+
+    'page.mobile.profileLinks.link.verification': 'Niveau {level}',
+    'page.mobile.profileLinks.link.2fa.enabled': 'Ingeschakeld',
+    'page.mobile.profileLinks.link.2fa.disabled': 'Uitgeschakeld',
+    'page.mobile.profileLinks.link.referral': 'Kopieer Link',
+
+    'page.mobile.profileColorTheme.theme.dark': 'Donker',
+    'page.mobile.profileColorTheme.theme.light': 'Licht',
+
+    'page.mobile.wallets.title': 'Spelers',
+
+    'page.mobile.profile.accountActivity.title': 'Account activiteit',
+    'page.mobile.profile.apiKeys.title': 'Mijn API keys',
+    'page.mobile.profile.theme.title': 'Thema',
+    'page.mobile.profile.language.title': 'Taal',
+    'page.mobile.profile.changePassword.title': 'Wijzig password',
+
+    'page.mobile.profile.accountActivity.action': 'Actie',
+    'page.mobile.profile.accountActivity.ip': 'IP adres',
+    'page.mobile.profile.accountActivity.result': 'Resultaat',
+
+    'page.mobile.profile.apiKeys.item.kid': 'Kid',
+    'page.mobile.profile.apiKeys.item.algorithm': 'Algorithme',
+    'page.mobile.profile.apiKeys.item.created': 'Aangemaakt',
+    'page.mobile.profile.apiKeys.item.updated': 'Gewijzigd',
+    'page.mobile.profile.apiKeys.item.delete': 'Verwijder',
+
+    'page.mobile.confirm.profile': 'Identiteit verificatie',
+    'page.mobile.confirm.document': 'ID verificatie',
+    'page.mobile.confirm.address': 'Woonadres verificatie',
+    'page.mobile.confirm.back': 'Terug',
+
+    'page.body.profile.header.account.content.twoFactorAuthentication':  'Two-factor authenticatie',
+
+    'page.profile.kyc.title': '2FA',
+
+    'page.mobile.twoFactorModal.title': '2FA Verificatie',
+    'page.mobile.twoFactorModal.subtitle': 'Voer 2fa code van de app in',
+    'page.mobile.twoFactorModal.send': 'Verstuur',
+
+    'page.mobile.createdApiKeyModal.title': 'Aangemaakt',
+    'page.mobile.createdApiKeyModal.confirm': 'bevestig',
+};
